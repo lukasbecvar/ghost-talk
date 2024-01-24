460b100254c4c02824cc15742102444d86d295a4
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-01-24 15:18:57 +0100

    [+] added login link to navbar

--- resources/views/sub-components/navigation.blade.php
+++ resources/views/sub-components/navigation.blade.php
@@ -1,6 +1,9 @@
 <nav class="navbar">
     <div class="navbar-left">
-        <a class="navbar-link" href="/">Home</a>
-        <a class="navbar-link" href="/about">About</a>
+        <a href="/">Home</a>
+        <a class="navbar-link" href="/about">About</a>        
+    </div>
+    <div class="navbar-right">
+        <a href="/login">Login</a>
     </div>
 </nav>
